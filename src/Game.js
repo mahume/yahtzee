576800e724347f38e970e6ff359c405eba97e2d8
mahume <michael.a.hume@gmail.com>
2020-02-12 22:10:44 -0700

    Basic layout styling

--- src/Game.js
+++ src/Game.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react'
 import Dice from "./Dice";
 import ScoreCard from "./ScoreCard";
 
+const numDice = 5;
+
 export default class Game extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      dice: Array.from({ length: numDice })
+    }
   }
 
   render() {
     return (
       <div className="Game">
-        <h1>Game</h1>
-        <Dice />
+        <header className="Game-header">
+          <h1 className="App-title">Yahtzee</h1>
+        </header>
         <ScoreCard />
       </div>
     )
