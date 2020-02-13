0cb21c0fc9ee3b3f1e7de202a6133270ed5da0d0
mahume <michael.a.hume@gmail.com>
2020-02-12 22:29:19 -0700

    Adding temp static labels

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 export default class RuleRow extends Component {
   render() {
     return (
-      <div>
-        Rule row
-      </div>
+      <tr className="RuleRow">
+        <td>Name</td>
+        <td>Score</td>
+      </tr>
     )
   }
 }
