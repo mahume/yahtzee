7824561285c0eb0abc209a9b7370f6a610512c84
mahume <michael.a.hume@gmail.com>
2020-02-12 22:27:19 -0700

    Adding tables to the rules section

---
+++ src/RuleRow.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class RuleRow extends Component {
+  render() {
+    return (
+      <div>
+        Rule row
+      </div>
+    )
+  }
+}
--- src/ScoreCard.js
+++ src/ScoreCard.js
@@ -1,10 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import RuleRow from "./RuleRow";
+import "./ScoreCard.css";
 
 export default class ScoreCard extends Component {
   render() {
     return (
-      <div>
-        Score Card
+      <div className="ScoreCard">
+        <div className="ScoreCard-section">
+          <h2>Upper</h2>
+          <table>
+            <tbody>
+              <RuleRow />
+            </tbody>
+          </table>
+        </div>
+        <div className="ScoreCard-section ScoreCard-section-lower">
+          <h2>Lower</h2>
+          <table>
+            <tbody>
+              <RuleRow />
+            </tbody>
+          </table>
+        </div>
       </div>
     )
   }
