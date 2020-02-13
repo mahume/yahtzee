8fea2ccb2f94ff8c733bf6d5cb74049a70a6ae70
mahume <michael.a.hume@gmail.com>
2020-02-12 21:57:50 -0700

    Adding die component

--- src/Dice.js
+++ src/Dice.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
+import Die from "./Die";
 
 export default class Dice extends Component {
   render() {
     return (
       <div className="Dice">
-        
+        <span>Dice</span>
+        <Die />
       </div>
     )
   }
---
+++ src/Die.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Die extends Component {
+  render() {
+    return (
+      <button>
+        Die
+      </button>
+    )
+  }
+}
