d72a201d4b3f925203f24fd985ddec974454961a
mahume <michael.a.hume@gmail.com>
2020-02-12 21:53:53 -0700

    Creating dice component

---
+++ src/Dice.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Dice extends Component {
+  render() {
+    return (
+      <div className="Dice">
+        
+      </div>
+    )
+  }
+}
--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react'
+import Dice from "./Dice";
 
 export default class Game extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {}
+  }
+
   render() {
     return (
-      <div>
+      <div className="Game">
         <h1>Game</h1>
+        <Dice />
       </div>
     )
   }
